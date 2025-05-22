fc9446b91f77605d64408def468ebb6fbfffc8d2
Mol10-1ce <anurag.chakraborty005.com>
2025-05-23 04:54:07 +0530

    Initial commit

---
+++ src/main/java/net/strange/doctormod/DoctorModClient.java
@@ -0,0 +1,10 @@
+package net.strange.doctormod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DoctorModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/strange/doctormod/DoctormodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.strange.doctormod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DoctormodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
