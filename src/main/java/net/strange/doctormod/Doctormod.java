1ffd2ba8678d9001b4f68d349fcbee02d8d15f38
Mol10-1ce <anurag.chakraborty005.com>
2025-05-23 04:56:01 +0530

    added a comment

--- src/main/java/net/strange/doctormod/Doctormod.java
+++ src/main/java/net/strange/doctormod/Doctormod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//very imp comm
 public class Doctormod implements ModInitializer {
 	public static final String MOD_ID = "doctormod";
 	// This logger is used to write text to the console and the log file.
